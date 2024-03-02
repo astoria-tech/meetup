32ac8c9dc7c7a5e4cd7ece76336e9521b18b27f4
Thomas Reggi <thomas@reggi.com>
2024-03-02 13:18:10 -0500

    deploy script

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
-});
+  integrations: [tailwind()],
+  site: 'https://reggi.github.io',
+  base: '/astoria-tech-meetup-feed',
+})
