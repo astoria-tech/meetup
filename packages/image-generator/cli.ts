ea7fce79e36db82ba2ce280edb0bba262bdc1872
Thomas Reggi <thomas@reggi.com>
2024-03-24 23:27:07 -0400

    fix pluralization of img outdir

--- packages/image-generator/cli.ts
+++ packages/image-generator/cli.ts
@@ -3,7 +3,10 @@ import {buildRelative} from './index'
 
 const program = new Command()
 
-program.option('-i, --image <string>', 'Image path').option('-d, --date <string>', 'Date string')
+program
+.option('-i, --image <string>', 'Image path')
+.option('-d, --date <string>', 'Date string')
+.option('-o, --out <string>', 'Output Path string')
 
 program.parse(process.argv)
 
@@ -11,9 +14,10 @@ const options = program.opts()
 
 const image = options.image && typeof options.image === 'string' ? options.image : null
 const date = options.date && typeof options.date === 'string' ? options.date : null
+const outputPath = options.out && typeof options.out === 'string' ? options.out : null
 
 if (image && date) {
-  buildRelative({image, date})
+  buildRelative({image, date, outputPath})
 } else {
   console.error('Both image and date flags are required.')
   process.exit(1)
