a24924207cd38cc808ce57e332726f2396c83af0
Thomas Reggi <thomas@reggi.com>
2024-04-02 01:48:02 -0400

    shipit 🕵️‍♀️ 🚢 2024-04-02 01:48:02 AM EDT 📴 👮

---
+++ shell/output-issue-key.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function makeoutput() {
+  local jsonData=$1
+  local jsonKey=$2
+  local variableName=$3
+
+  # Use eval to handle dynamic variable names
+  local value=$(echo "$jsonData" | jq -r --arg key "$jsonKey" '.[$key].text')
+  eval $variableName="'$value'"
+  
+  # Output to GitHub and echo
+  echo "${variableName}=${value}" >> "$GITHUB_OUTPUT"
+  echo "$value"
+}
