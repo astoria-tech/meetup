f9724bfcd60d1209e8dca3ff598fb0e79c8034f2
Hannah Nordgren <hey@hereshannahs.info>
2024-04-18 01:46:45 -0400

    perf: add initial palette and WIP style variables
    perf: abstract default theme
    
    perf: temporary color swatches added
    
    refactor: move fonts to themes and remove example code
    
    refactor: clean up style.css, add postcss
    
    refactor: file organization, themes

---
+++ src/styles/themes/base.ts
@@ -0,0 +1,45 @@
+import { 
+    baseColors, 
+    primaryGradient, 
+    secondaryGradient,
+    tertiaryGradient 
+} from "./config/themeVariables";
+
+// todo: gradients
+export const colors = {
+        'black': baseColors.black,
+        'success': baseColors.success,
+        'warning': baseColors.warning,
+        'error': baseColors.error,
+        'link-base': baseColors.link.base,
+        'link-hover': baseColors.link.light,
+        'link-active': baseColors.link.light,
+        'link-focus': baseColors.link.light,
+        'link-visited': baseColors.link.dark,
+        'link-disabled': baseColors.link.dark,
+        'primary': primaryGradient.base,
+        'primary-100': primaryGradient[100],
+        'primary-200': primaryGradient[200],
+        'primary-300': primaryGradient[300],
+        'primary-400': primaryGradient[400],
+        'primary-500': primaryGradient[500],
+        'secondary': secondaryGradient.base,
+        'secondary-100': secondaryGradient[100],
+        'secondary-200': secondaryGradient[200],
+        'secondary-300': secondaryGradient[300],
+        'secondary-400': secondaryGradient[400],
+        'secondary-500': secondaryGradient[500],
+        'tertiary': tertiaryGradient.base,
+        'tertiary-100': tertiaryGradient[100],
+        'tertiary-200': tertiaryGradient[200],
+        'tertiary-300': tertiaryGradient[300],
+        'tertiary-400': tertiaryGradient[400],
+        'tertiary-500': tertiaryGradient[500],
+};
+
+export const fontFamily = {
+    'sans': ['NotoSans', 'Arial', 'Helvetica', 'sans-serif'],
+    'sans-bold': ['NotoSans-bold', 'Arial', 'Helvetica', 'sans-serif'],
+    'serif': ['PlayFair', 'Times New Roman', 'serif'],
+    'serif-bold': ['PlayFair-Bold', 'Times New Roman', 'serif'],
+  };
---
+++ src/styles/themes/config/themeVariables.ts
@@ -0,0 +1,45 @@
+//These color variables are the base for which theme variables can be assigned or referenced directly.
+
+export const linkPalette = {
+    base: '#4d86f7',
+    dark: '#3d6bc5',
+    light: '#709ef8',
+};
+
+export const baseColors = {
+    black: '#0D0D0D',
+    white: '#DCDEF2',
+    gray: '#6D6D6D',
+    success: '#4c8a68',
+    warning: '#F7BE4D',
+    error: '#F75C4D',
+    link: linkPalette
+};
+
+export const primaryGradient = {
+    base: '#315943',
+    100: '#456955',
+    200: '#5a7a68',
+    300: '#6e8a7b',
+    400: '#839b8e',
+    500: '#98aca1',
+};
+
+export const secondaryGradient = {
+    base: '#D9D2C5',
+    100: '#dcd6ca',
+    200: '#e0dbd0',
+    300: '#e4dfd6',
+    400: '#e8e4dc',
+    500: '#ece8e2',
+};
+
+export const tertiaryGradient = {
+    base: '#F2B3B3',
+    100: '#f3baba',
+    200: '#f4c2c2',
+    300: '#f5c9c9',
+    400: '#f7d1d1',
+    500: '#f8d9d9',
+};
+
