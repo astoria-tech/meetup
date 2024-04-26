7842b76b0ddcd27a1c07d3239e1f5e6078160d0a
Hannah Nordgren <hey@hereshannahs.info>
2024-04-25 23:01:07 -0400

    refactor: run style:fix, fixed styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,12 +1,12 @@
 /** @type {import('tailwindcss').Config} */
-import {colors, fontFamily} from './src/styles/themes/base';
+import {colors, fontFamily} from './src/styles/themes/base'
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
   theme: {
     extend: {
       colors,
-      fontFamily
+      fontFamily,
     },
   },
   plugins: [],
