b66918ae95a88a9622a4a269b9e80dd42773c829
Thomas Reggi <thomas@reggi.com>
2024-05-04 04:48:45 -0400

    fix undefined

--- src/markdown/event-markdown.ts
+++ src/markdown/event-markdown.ts
@@ -3,7 +3,7 @@ import type {HydratedFeedItemPreHtml} from '../types'
 export default ({event, permalink}: HydratedFeedItemPreHtml) => `
 # [Save the date - ${event.data.title}](${permalink})
 
-${event.data.meetupImage && `<img alt="event banner for ${event.data.date}" src="${event.data.meetupImage}" style="width:500px"/>`}
+${event.data.meetupImage ? `<img alt="event banner for ${event.data.date}" src="${event.data.meetupImage}" style="width:500px"/>` : ''}
 
 Our next Event will be on ${event.data.date}
 
